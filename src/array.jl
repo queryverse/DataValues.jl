059b90187012670d52c5f944604d807ecb4c82f0
David Anthoff <anthoff@berkeley.edu>
2017-08-15 11:12:21 -0700

    Remove some experimental hacks

--- src/array.jl
+++ src/array.jl
@@ -33,12 +33,6 @@ end
 const DataValueVector{T} = DataValueArray{T, 1}
 const DataValueMatrix{T} = DataValueArray{T, 2}
 
-# Array{T,N}(d::NTuple{N,Int}) where {T<:DataValue,N} =DataValueArray{eltype(T),N}(d)
-# Array{T,1}(m::Int) where {T<:DataValue} = DataValueArray{eltype(T),1}(m)
-# Array{T,2}(m::Int, n::Int) where {T<:DataValue} = DataValueArray{eltype(T),2}(m,n)
-# Array{T,3}(m::Int, n::Int, o::Int) where {T<:DataValue} = DataValueArray{eltype(T),3}(m,n,o)
-# Array{T,N}(d::Vararg{Int,N}) where {T<:DataValue,N} = DataValueArray{eltype(T),N}(d)
-
 function Base.convert(::Type{DataValueArray}, a::AbstractArray{T,N}) where {T,N}
     DataValueArray{T,N}(a, fill(false, size(a)))
 end
