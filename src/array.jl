4e5f53c0e0a9225a297b73403076c221d1471f77
David Anthoff <anthoff@berkeley.edu>
2017-08-16 17:22:04 -0700

    Add some promote_type methods

--- src/array.jl
+++ src/array.jl
@@ -304,3 +304,8 @@ function Base.deleteat!(X::DataValueVector, inds)
     deleteat!(X.isnull, inds)
     return X
 end
+
+Base.promote_rule(::Type{DataValueArray{T,N}}, ::Type{Array{T,N}}) where {T,N} = DataValueArray{T,N}
+Base.promote_rule(::Type{Array{T,N}}, ::Type{DataValueArray{T,N}}) where {T,N} = DataValueArray{T,N}
+Base.promote_rule(::Type{DataValueArray{T,N}}, ::Type{Array{S,N}}) where {T,S,N} = DataValueArray{Base.promote_type(T,S),N}
+Base.promote_rule(::Type{Array{S,N}}, ::Type{DataValueArray{T,N}}) where {T,S,N} = DataValueArray{Base.promote_type(T,S),N}
